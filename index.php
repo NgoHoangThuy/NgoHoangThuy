138662427df21e62fb261ea87b572d6e595d2a4b
NgoHoangThuy <ngohoangthuy2001@gmail.com>
2021-09-15 22:45:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>Ngô Hoàng Thủy</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Xin chào!! Thủy 
+        <h1 title="HTML, CSS từ zero đến Hero" >Học về attribute trong HTML</h1>
+        <a href="https://fullstack.edu.vn/">Học HTML tại F8</a>
+        <img src="https://img.youtube.com/vi/0SJE9dYdpps/sddefault.jpg" width="250" title="Lập trình Javascript cơ bản">
+
+    </body>
+</html>
